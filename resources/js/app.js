3cb195d6bfe0bdce3ff32ce5677ad31f928018c6
Yilun-Fu <fuyilunpower@163.com>
2024-07-04 17:24:28 +0800

    add File Uploading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,37 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+Vue.use(BootstrapVue);
 
+// 注册 Vue 组件
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.component('success-alert', require('./components/SuccessAlert.vue').default);
 
 const app = new Vue({
     el: '#app',
+    data: {
+        showAlert: false,
+        alertMessage: '',
+    },
+    methods: {
+        handleUpload(event) {
+            event.preventDefault();
+            let formData = new FormData(event.target);
+
+            axios.post('/upload', formData)
+                .then(response => {
+                    this.alertMessage = 'Files uploaded successfully.';
+                    this.showAlert = true;
+                })
+                .catch(error => {
+                    this.alertMessage = 'Failed to upload files.';
+                    this.showAlert = true;
+                });
+        }
+    }
 });
---
+++ resources/js/components/SuccessAlert.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-alert
+        :variant="variant"
+        show
+        dismissible
+        @dismissed="dismiss"
+    >
+        {{ message }}
+    </b-alert>
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            required: true
+        },
+        variant: {
+            type: String,
+            default: 'success'
+        }
+    },
+    methods: {
+        dismiss() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
